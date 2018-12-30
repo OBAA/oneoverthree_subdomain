31bd4d7b9dd07f8fa242822c387fc001bb48ad90
OBAA <agbanabolu@gmail.com>
2018-12-31 00:01:05 +0100

    SKU pre-save config

--- store/models.py
+++ store/models.py
@@ -132,7 +132,7 @@ class ProductManager(models.Manager):
 class Product(models.Model):
     title               = models.CharField(max_length=120, )
     slug                = models.SlugField(max_length=120, blank=True, unique=True)
-    sku                 = models.CharField(max_length=120, blank=True)
+    sku                 = models.CharField(max_length=120, blank=True)  # , unique=True)
     product_type        = models.ForeignKey(ProductWeight, default=0.7,
                                   help_text='For calculating shipping cost', null=True)
     store               = TreeForeignKey(Store, related_name='products', default=None, null=True)
@@ -180,10 +180,7 @@ def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
-    # if not instance.sku:
-    #     instance.sku = unique_sku_generator(instance)
-
-    if instance.sku == "1":
+    if not instance.sku:
         instance.sku = unique_sku_generator(instance)
 
     if not instance.price:
