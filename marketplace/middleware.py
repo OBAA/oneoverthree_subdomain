faeabe4bef9fc6801fb8f53236d8c21ee920b553
OBAA <agbanabolu@gmail.com>
2019-01-04 19:46:23 +0100

    Fixing Middleware

--- marketplace/middleware.py
+++ marketplace/middleware.py
@@ -39,44 +39,45 @@ class StoreSubdomainMiddleware(MiddlewareMixin):
             store_slug = pieces[0].lower()
             new_path = "/marketplace/{0}/".format(store_slug)
 
-            print("--- Try block 1 ---")
-            print(new_path)
-            try:
-                print("Try 1")
-                match = resolve(new_path, marketplace_urls)
-                print("tried")
-                if match:
-                    print(match.url_name, match.kwargs, match.namespace)
-
-            except Resolver404:
-                try:
-                    print("try 2")
-                    # The slashes are not being appended before getting here
-                    match = resolve(u"{0}/".format(path), marketplace_urls)
-                    if match:
-                        # print(match.url_name, match.kwargs, match.namespace)
-                        return HttpResponseRedirect("{0}://{1}{2}".format(scheme, domain, new_path))
-                except Resolver404:
-                    print("break 1")
-                    # pass
-                    return redirect(redirect_path)
-
-            # print("--- Try block 2 ---")
+            # print("--- Try block 1 ---")
+            # print(new_path)
             # try:
-            #     print("try 3")
-            #     store = Store.objects.get(slug=store_slug)
-            #     print(store)
+            #     print("Try 1")
+            #     match = resolve(new_path, marketplace_urls)
+            #     print("tried")
+            #     if match:
+            #         # print(match.url_name, match.kwargs, match.namespace)
+            #         return HttpResponseRedirect("{0}://{1}{2}".format(scheme, domain, new_path))
             #
-            # except Store.MultipleObjectsReturned:
-            #     qs = Store.objects.filter(slug=store_slug)
-            #     store = qs.first()
-            #
-            # except Store.DoesNotExist:
-            #     print("break 2")
-            #     msg = "Store not found. shop our products."
-            #     messages.warning(request, msg)
-            #     return redirect(redirect_path)
-            # if store:
-            #     request.domain = store
-            #     return HttpResponseRedirect("{0}://{1}{2}".format(scheme, domain, new_path))
+            # except Resolver404:
+            #     try:
+            #         print("try 2")
+            #         # The slashes are not being appended before getting here
+            #         match = resolve(u"{0}/".format(path), marketplace_urls)
+            #         if match:
+            #             # print(match.url_name, match.kwargs, match.namespace)
+            #             return HttpResponseRedirect("{0}://{1}{2}".format(scheme, domain, new_path))
+            #     except Resolver404:
+            #         print("break 1")
+            #         # pass
+            #         return redirect(redirect_path)
+
+            print("--- Try block 2 ---")
+            try:
+                print("try 3")
+                store = Store.objects.get(slug=store_slug)
+                print(store)
+
+            except Store.MultipleObjectsReturned:
+                qs = Store.objects.filter(slug=store_slug)
+                store = qs.first()
+
+            except Store.DoesNotExist:
+                print("break 2")
+                msg = "Store not found. shop our products."
+                messages.warning(request, msg)
+                return redirect(redirect_path)
+            if store:
+                request.domain = store
+                return HttpResponseRedirect("{0}://{1}{2}".format(scheme, domain, new_path))
 
